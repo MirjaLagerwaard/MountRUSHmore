63f0bd480e85a3a04b0b5b1fac0cd83ff8c9e074
MirjaLagerwaard <mirja_1994@hotmail.com>
2016-11-26 15:41:54 +0100

    Finished and tested possibleMoves, added print/make/updateBoard

--- board.py
+++ board.py
@@ -1,4 +1,5 @@
 import pprint
+import numpy as np
 
 class Board(object):
     """
@@ -8,134 +9,151 @@ class Board(object):
         """
         Initialize board.
         """
+
         self.width = width
         self.height = height
         self.y_exit = y_exit
-        self.board = [['_' for j in range(self.height)] for i in range(self.width)]
+        self.board = [['_'] * self.height for _ in range(self.width)]
+
+    def printBoard(self):
+        """
+        Prints the board.
+        """
+
+        # transpose board to print x and y correctly
+        board = [[j[i] for j in self.board] for i in range(len(self.board[0]))]
+
+        # print board
+        for element in board:
+            print " ".join(element)
 
     def makeBoard(self, hor_auto, ver_auto):
         """
-        Prints the begin state of the board.
+        Makes the beginstate of the board.
         """
 
-        for vehicle in hor_auto:
-            for i in range(0, int(vehicle.size)):
-                self.board[int(vehicle.y)][int(vehicle.x) - i] = vehicle.ID
-        for vehicle in ver_auto:
-            for i in range(0, int(vehicle.size)):
-                self.board[int(vehicle.y) - i][int(vehicle.x)] = vehicle.ID
+        # iterate over each verhicle in list 'hor_auto'
+        for ID, vehicle in hor_auto.iteritems():
+            # set vehicle ID on each position of the vehicle
+            for i in range(0, vehicle.size):
+                self.board[vehicle.x - i][ vehicle.y] = ID
 
-        for element in self.board:
-            print (" ".join(element))
-        print '\n'
+        # iterate over each verhicle in list 'ver_auto'
+        for ID, vehicle in ver_auto.iteritems():
+            # set vehicle ID on each position of the vehicle
+            for i in range(0, vehicle.size):
+                self.board[vehicle.x][vehicle.y - i] = ID
+
+        # prints the beginstate of the board
+        self.printBoard()
 
     def updateBoard(self, hor_auto, ver_auto, ID, x, y):
+        """
+        Update the board and the hor_auto or ver_auto dict.
+        """
 
-        # for x_board in range(self.width - 1):
-        #     for y_board in range(self.height - 1):
-        #         if self.board[y_board][x_board] == ID:
-        #             self.board[y_board][x_board] = '_'
-        #     self.board[y][x] = ID
-        #     self.board[y][x - 1] = ID
-        #
-        # for element in self.board:
-        #     print (" ".join(element))
-        # print '\n'
+        for x_board in range(self.width - 1):
+            for y_board in range(self.height - 1):
+                if self.board[x_board][y_board] == ID:
+                    self.board[x_board][y_board] = '_'
+
+        if ID in hor_auto:
+            hor_auto[ID].x = x
+
+            self.board[x][y] = ID
+            self.board[x - 1][y] = ID
+
+        elif ID in ver_auto:
+            ver_auto[ID].y = y
+
+            self.board[x][y] = ID
+            self.board[x][y - 1] = ID
+
+        self.printBoard()
 
     def possibleMoves(self, hor_auto, ver_auto):
         """
-        Returns all possible moves of a specific state of the board.
+        Returns all possible moves of a specific state of the board as a dict.
         """
 
         children = {}
-        moves = []
-        """
-        Movement of horizontal cars TO RIGHT
-        """
 
+        # MOVEMENT OF HORIZONTAL CARS
         # iterate over each vehicle in list 'hor_auto'
-        for vehicle in hor_auto:
-            # iterate over each possible x-position untill wall is reached
-            for i in range(self.width - 1):
+        for ID, vehicle in hor_auto.iteritems():
+            # iterate over each possible x-position untill RIGHT wall is reached
+            for i in range(vehicle.x + 1, self.width):
                 # check if position in front of vehicle is empty
-                if self.board[vehicle.y][vehicle.x + i] == "_":
-                    # add possible position to list 'moves'
-                    moves.append(vehicle.x + i)
-                # vehicle is not alowed to move, a car or truck is in his way
-                else:
-                    break
-            # hash all possible moves in dictionary children with key vehicle.ID
-            children[vehicle.ID] = moves
-
-        #'clear list 'moves'
-        moves = []
-
-        """
-        Movement of horizontal vehicles TO LEFT
-        """
-
-        # iterate over each x-position on board until wall is reached
-        for vehicle in hor_auto
-            for i in range etX(), (self.size - 1), -1):
-            # check backwards moving if vehicle is a car
-            if self.size == '2':
-                if board[Vehicle.getX() - 2, Vehicle.getY()] == "_":
-                    childs.append(board)
-                # vehicle is not alowed to move, a car or truck is in his way
-                else:
-                    break
-            # check backwards moving if vehicle is a truck
-            elif self.size == '3':
-                if board[Vehicle.getX() - 3, Vehicle.getY()] == "_":
-                    childs.append(board)
+                if self.board[i][vehicle.y] == "_":
+                    # store the coordinates as a tuple
+                    coordinate = (i, vehicle.y)
+                    # hash the possible child to the dict 'children'
+                    if ID in children:
+                        children[ID].append(coordinate)
+                    else:
+                        children[ID] = [coordinate]
                 # vehicle is not alowed to move, a car or truck is in his way
                 else:
                     break
 
-        """
-        Movement of vertical vehicles DOWNWARDS
-        """
-
-        # iterate over each y-position on board until wall is reached
-        for y_pos in ver_auto(Vehicle.getY(), (self.height - 1), 1):
-            # check if position in front of vehicle is empty
-            if (board[Vehicle.getX(), Vehicle.getY() + 1] == "_"):
-                childs.append(board)
-            # vehicle is not alowed to move, a car or truck is in his way
-            else:
-                break
-
-        """
-        Movement of vertical vehicles UPWARDS
-        """
-
-        # iterate over each y-position on board until wall is reached
-        for y_pos in ver_auto(Vehicle.getY(), (self.size - 1), -1):
-            # check backwards moving if vehicle is a car
-            if self.size == '2':
-                if board[Vehicle.getX() - 2, Vehicle.getY()] == "_":
-                    childs.append(board)
-                # vehicle is not alowed to move, a car or truck is in his way
-                else:
-                    break
-            # check backwards moving if vehicle is a truck
-            elif self.size == '3':
-                if board[Vehicle.getX() - 3, Vehicle.getY()] == "_":
-                    childs.append(board)
+            # iterate over each possible x-position untill LEFT wall is reached
+            for i in range (vehicle.x - 1, vehicle.size - 2, -1):
+                # check if position in front of vehicle is empty
+                if self.board[i - (vehicle.size - 1)][vehicle.y] == "_":
+                    # store the coordinates as a tuple
+                    coordinate = (i, vehicle.y)
+                    # hash the possible child to the dict 'children'
+                    if ID in children:
+                        children[ID].append(coordinate)
+                    else:
+                        children[ID] = [coordinate]
                 # vehicle is not alowed to move, a car or truck is in his way
                 else:
                     break
 
-        print childs
-        return childs
+        # MOVEMENT OF VERTICAL CARS
+        # iterate over each vehicle in list 'ver_auto'
+        for ID, vehicle in ver_auto.iteritems():
+            # iterate over each possible y-position untill LOWER wall is reached
+            for i in range(vehicle.y + 1, self.height):
+                # check if position in front of vehicle is empty
+                if self.board[vehicle.x][i] == "_":
+                    # store the coordinates as a tuple
+                    coordinate = (vehicle.x, i)
+                    # hash the possible child to the dict 'children'
+                    if ID in children:
+                        children[ID].append(coordinate)
+                    else:
+                        children[ID] = [coordinate]
+                # vehicle is not alowed to move, a car or truck is in his way
+                else:
+                    break
+
+            # iterate over each possible y-position untill UPPER wall is reached
+            for i in range (vehicle.y - 1, vehicle.size - 2, -1):
+                # check if position in front of vehicle is empty
+                if self.board[vehicle.x][i - (vehicle.size - 1)] == "_":
+                    # store the coordinates as a tuple
+                    coordinate = (vehicle.x , i)
+                    # hash the possible child to the dict 'children'
+                    if ID in children:
+                        children[ID].append(coordinate)
+                    else:
+                        children[ID] = [coordinate]
+                # vehicle is not alowed to move, a car or truck is in his way
+                else:
+                    break
+
+        # return the dict 'children' with all the possible moves of each vehicle
+        return children
 
     def isSolution(self):
         """
         Returns true when red car is at the EXIT
         """
         for verhicles in hor_auto:
-            if self.y_exit == Vehicle.getY():
-                if Vehicle.getX() == self.width - 1:
+            if self.y_exit == vehicle.y:
+                if vehicle.x == self.width - 1:
                     return True
 
     def BreadthFirstSearch(board):
--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import csv
 from board import *
 
-hor_auto = []
-ver_auto = []
+hor_auto = {}
+ver_auto = {}
 
 with open("1_vehicle_test.csv", "rb") as fileVehicle:
     reader_vehicles = csv.reader(fileVehicle, delimiter=',')
     for vehicle in reader_vehicles:
         if vehicle[0] == "H":
-            hor_auto.append(Vehicle(vehicle[0], vehicle[1], vehicle[2], vehicle[3], vehicle[4]))
+            hor_auto[vehicle[1]] = Vehicle(vehicle[0], vehicle[1], int(vehicle[2]), int(vehicle[3]), int(vehicle[4]))
         if vehicle[0] == "V":
-            ver_auto.append(Vehicle(vehicle[0], vehicle[1], vehicle[2], vehicle[3], vehicle[4]))
+            ver_auto[vehicle[1]] = Vehicle(vehicle[0], vehicle[1], int(vehicle[2]), int(vehicle[3]), int(vehicle[4]))
 
-Board1 = Board(3, 3, 1)
+Board1 = Board(4, 6, 1)
 initBoard1 = Board1.makeBoard(hor_auto, ver_auto)
-move1 = Board1.updateBoard(hor_auto, ver_auto, vehicle[1], 2, 0)
+# move1 = Board1.updateBoard(hor_auto, ver_auto, vehicle[1], 1, 2)
+print Board1.possibleMoves(hor_auto, ver_auto)
