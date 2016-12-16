59a8d3c0663669602295f714ea4ff1f096eec3bd
MirjaLagerwaard <mirja_1994@hotmail.com>
2016-12-16 11:00:21 +0100

    Implemented Depth-Firt, not fully tested

--- algorithm.py
+++ algorithm.py
@@ -5,234 +5,273 @@ import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 
+
 def toString(vehicles):
     """
     Makes a string of the state of the board.
     """
 
-    # make string
     s = ""
 
     # iterate over each vehicle in the array with vehicles
     for _, vehicle in vehicles.iteritems():
-        # store the x-coordinate when
+        # store the x-coordinate as a string when direction of vehicle is horizontal
         if vehicle.dir == 'H':
             s += str(vehicle.x)
-
+        # store the y-coordinate as a string when direction of the vehicle is vertical
         if vehicle.dir == 'V':
             s += str(vehicle.y)
 
     return s
+
 
 def updateArray (ID, x, y, vehicles):
     """
     Updates the array of vehicles.
     """
 
+    # update the x-coordinate of the vehicle when direction is horizontal
     if vehicles[ID].dir == 'H':
         vehicles[ID].x = x
+    # update the y-coordinate of the vehicle when direction is vertical
     elif vehicles[ID].dir == 'V':
         vehicles[ID].y = y
 
     return vehicles
 
+
 def Random(board):
     """
-    TODO
+    Picks a random possible move untill solution is found.
     """
 
+    # create list for the amount of moves
     plot_moves = []
+    # make copy of the original board
     original_board = copy.deepcopy(board)
+    # set the amount of desired iterations
+    iterations = 10
 
-    for i in range(10):
+    # loop iterations times
+    for i in range(iterations):
 
+        # print the current iteration
         print "Iteration: ", i
-        total_moves = 1
-        new_list = []
-        archive_list = []
-        archive_length = 1
-        counter_archive = 0
+        # reset board to original board
         board = copy.deepcopy(original_board)
+        # set counter for the amount of moves to 1, because moving the red car to the EXIT is also a move
+        total_moves = 1
+        # create list for converting the list of moves
+        converted_list = []
+        # create list as archive
+        archive_list = []
 
-        while not board.isSolution() and total_moves < 175:
+        # loop untill the solution of the board is found
+        while not board.isSolution():
 
+            # get all possible moves
             moves = board.possibleMoves()
 
-            for i in range (0, len(moves)):
-                for j in range (0, len(moves.values()[i])):
-                    new_list.append((moves.values()[i][j], moves.keys()[i]))
+            # loop over every vehicle which can move (using the ID of the vehicle)
+            for ID in range (0, len(moves)):
+                # loop over every move one vehicle can make
+                for move in range (0, len(moves.values()[ID])):
+                    # store every single move of the vehicle as tuple in converted list
+                    converted_list.append((moves.values()[ID][move], moves.keys()[ID]))
 
-            #Total random:
-            random_move, ID = random.choice(new_list)
+            # pick a random possible move
+            random_move, ID = random.choice(converted_list)
+            # make seperate variables of the x- and y-coordinate of the random move
             x, y = random_move
 
-            # Pseudorandom:
-            # ID = random.choice(moves.keys())
-            # x, y = random.choice(moves[ID])
-
+            # update board after the random move
             board.updateBoard(ID, x, y, board.vehicles)
+            # update the vehicle array after the random move
             board.vehicles = updateArray(ID, x, y, board.vehicles)
 
+            # update the amount of moves
             total_moves += 1
-
-            new_list = []
+            # clear the list with possible moves
+            converted_list = []
 
         if board.isSolution():
+            # print the total amount of moves
             print str(total_moves) + " moves were needed."
+            # store the sample of the amount of moves in list plot moves
             plot_moves.append(total_moves)
 
+    # sort the list with the samples of moves
     plot_moves.sort()
+    # plot the list with the samples of moves
     print plot_moves
 
+    # make histogram of the sample
     n, bins, patches = plt.hist(plot_moves, 15, normed=1, facecolor='grey', alpha=0.75)
+    # show histogram
     plt.show()
 
 
-def DepthFirstSearch(board):
+def DepthFirstSearch(board, max_depth, solution_list):
     """
-    Depth-first search (DFS) is an algorithm for traversing or
-    searching tree or graph data structures. One starts at the
-    root (selecting some arbitrary node as the root in the case
-    of a graph) and explores as far as possible along each branch
-    before backtracking.
+    Depth-first search (DFS) is an algorithm for traversing or searching tree or
+    graph data structures. One starts at the root (selecting some arbitrary node
+    as the root in the case of a graph) and explores as far as possible along
+    each branch before backtracking.
     """
 
-    stack = []
-    archive = {toString(board.vehicles): 0}
-    bound = 175
+    # create a list as stack and store the beginstate array of vehicles and the depth as tuple
+    stack = [(board.vehicles, 0)]
+    # create archive and hash the beginstate array of vehicles
+    archive = {(toString(board.vehicles),0): 0}
 
-    stack.append(board.vehicles)
+    # loop untill stack is empty
+    while len(stack) > 0:
 
-    while stack > 0 and bound < 175:
+        # get the first board of the stack (i.e. parent)
+        parent_vehicles, depth = stack.pop()
 
-        parent_vehicles = stack.pop()
+        if depth >= max_depth:
+            continue
+
+        # reset board with board from the stack
         board.makeBoard(parent_vehicles)
+        # get all the possible moves of this board (i.e. all the children)
         moves = board.possibleMoves()
 
         # iterate over each possible move
         for ID, move_list in moves.iteritems():
-            # iterate over each x- and y-coordinate
+            # iterate over each x- and y-coordinate of possible move
             for (x, y) in move_list:
-                # make a copy of the child state
+                # make a copy of the parent
                 child_vehicles = copy.deepcopy(parent_vehicles)
-
-                # update array
+                # update array after move
                 child_vehicles = updateArray(ID, x, y, child_vehicles)
 
-                if toString(child_vehicles) in archive:
+                if (toString(child_vehicles), depth + 1) in archive:
                     continue
 
+                print "Depth: ", depth, "Stack: ", len(stack), "Archive: ", len(archive), "Max-depth: ", max_depth
+
+                # hash the parent state with as key the child state
+                archive[(toString(child_vehicles), depth + 1)] = parent_vehicles
+                # update board after move
                 board.updateBoard(ID, x, y, parent_vehicles)
 
                 if board.isSolution():
-                    # hoeveel stappen heeft het gekost = in welke diepte zit je om bij deze oplossing te komen?
-                    # stel dit in als nieuwe bound (bound = nieuw getal)
-                    # ga verder
+                    print "Firework, Champagne, Confetti!"
 
-                    # print "Firework, Champagne, Confetti!"
-                    #
-                    # solution_list = []
-                    #
-                    # solution_list.append(copy.deepcopy(board))
-                    #
-                    # crawler = archive[toString(child_vehicles)]
-                    # while archive[toString(crawler)] != 0:
-                    #     board.makeBoard(crawler)
-                    #     solution_list.append(copy.deepcopy(board))
-                    #     crawler = archive[toString(crawler)]
-                    #
-                    # for i, boards in enumerate(solution_list[::-1]):
-                    #     print "Moves: ", i
-                    #     boards.printBoard()
-                    #
-                    # print "Total moves: ", i + 2
-                    # return
+                    # create a list for all the moves to the solution list
+                    solution_list = []
+                    # append copy of the solution board
+                    solution_list.append(copy.deepcopy(board))
+                    # create crawler as hash of child vehicles
+                    crawler = archive[(toString(child_vehicles), depth + 1)]
 
-                # add the child state to archive
-                archive[toString(child_vehicles)] = archive[toString(parent_vehicles)] + 1
+                    # iterate over archive untill beginstate is reached
+                    while archive[(toString(crawler), depth)] != 0:
+                        # make a board of the parent state
+                        board.makeBoard(crawler)
+                        # store the parent state in the solution list
+                        solution_list.append(copy.deepcopy(board))
+                        # update crawler
+                        crawler = archive[(toString(crawler), depth)]
+                        depth -= 1
+
+                    # total moves = i + 2, because the beginstate and moving the red car to the EXIT are also moves
+                    return len(solution_list), solution_list
 
                 # add the children of the parent state to the queue
-                stack.append(child_vehicles)
+                stack.append((child_vehicles, depth + 1))
 
+                # update the old x-coordinate
                 old_x = parent_vehicles[ID].x
+                # update the old y-coordinate
                 old_y = parent_vehicles[ID].y
+                # update board
                 board.updateBoard(ID, old_x, old_y, child_vehicles)
 
     print "No solution found."
+    return -1, solution_list
 
 
 def BreadthFirstSearch(board):
     """
-    Breadth-first search (BFS) is an algorithm for traversing or searching
-    tree or graph data structures. It starts at the tree root (or some
-    arbitrary node of a graph, sometimes referred to as a 'search key'[1])
-    and explores the neighbor nodes first, before moving to the next level
-    neighbors.
+    Breadth-first search (BFS) is an algorithm for traversing or searching tree
+    or graph data structures. It starts at the tree root (or some arbitrary node
+    of a graph, sometimes referred to as a 'search key'[1]) and explores the
+    neighbor nodes first, before moving to the next level neighbors.
     """
-    # make the archive (as a dict) and the queue (as a list)
-    archive = {toString(board.vehicles): 0} # hash van startbord (board1)
-    queue = Queue.Queue()
 
-    queue.put(board.vehicles) # stopt alleen nog maar de auto's in de queue
+    # create archive and hash the beginstate array of vehicles
+    archive = {toString(board.vehicles): 0}
+    # create queue and and store the beginstate array of vehicles
+    queue = Queue.Queue(board.vehicles)
 
-    # while there are items in the queue
+    # loop untill queue is empty
     while queue.qsize():
-        # pop the first item of the queue (FIFO: first in, first out)
-        parent_vehicles = queue.get() # pak de eerste lijst van auto's die in de queue staat
 
-        # print "Layer: ", archive[toString(parent_vehicles)], " Archive_len:", len(archive)
-
-        board.makeBoard(parent_vehicles) # reset het bord voor we possibleMoves gaan doen
-
-        # get all the possible moves of this parent state, i.e. the children
+        # get the first board of the queue (i.e. parent)
+        parent_vehicles = queue.get()
+        # reset board with board from the queue
+        board.makeBoard(parent_vehicles)
+        # get all the possible moves of this board (i.e. all the children)
         moves = board.possibleMoves()
 
         # iterate over each possible move
         for ID, move_list in moves.iteritems():
-            # iterate over each x- and y-coordinate
+            # iterate over each x- and y-coordinate of possible move
             for (x, y) in move_list:
-
-                # make a copy of the child state
+                # make a copy of the parent
                 child_vehicles = copy.deepcopy(parent_vehicles)
-
-                # update array
+                # update array after move
                 child_vehicles = updateArray(ID, x, y, child_vehicles)
 
-                # print len(archive)
-
-                # if the child is in the archive, break out of loop
                 if toString(child_vehicles) in archive:
                     continue
 
+                # hash the parent state with as key the child state
                 archive[toString(child_vehicles)] = parent_vehicles
-
+                # update board after move
                 board.updateBoard(ID, x, y, parent_vehicles)
-                # if the child is the solution, print party and how many moves were needed, and return
+
                 if board.isSolution():
                     print "Firework, Champagne, Confetti!"
 
+                    # create a list for all the moves to the solution list
                     solution_list = []
-
+                    # append copy of the solution board
                     solution_list.append(copy.deepcopy(board))
-
+                    # create crawler as hash of child vehicles
                     crawler = archive[toString(child_vehicles)]
+
+                    # iterate over archive untill beginstate is reached
                     while archive[toString(crawler)] != 0:
+                        # make a board of the parent state
                         board.makeBoard(crawler)
+                        # store the parent state in the solution list
                         solution_list.append(copy.deepcopy(board))
+                        # update crawler
                         crawler = archive[toString(crawler)]
 
+                    # iterate backwards over de parent boards in solution list
                     for i, boards in enumerate(solution_list[::-1]):
-                        print "Moves: ", i
+                        # print the number of moves
+                        print "s: ", i
+                        # print the board
                         boards.printBoard()
 
+                    # total moves = i + 2, because the beginstate and moving the red car to the EXIT are also moves
                     print "Total moves: ", i + 2
                     return
 
                 # add the children of the parent state to the queue
                 queue.put(child_vehicles)
 
+                # update the old x-coordinate
                 old_x = parent_vehicles[ID].x
+                # update the old y-coordinate
                 old_y = parent_vehicles[ID].y
+                # update board
                 board.updateBoard(ID, old_x, old_y, child_vehicles)
 
     print "No solution found."
--- main.py
+++ main.py
@@ -13,24 +13,24 @@ vehicles = {}
 # fp = "vehicles_6x6_1.csv"
 
 # Breadth 15 zetten
-fp = "vehicles_6x6_2.csv"
+# fp = "vehicles_6x6_2.csv"
 
 # Breadth 21 zetten
 # fp = "vehicles_6x6_3.csv"
 
-# Card 9 zetten -> Breadth 9 zetten -> Depth ...
+# Card 9 zetten -> Breadth 9 zetten -> Depth 13
 # fp = "vehicles_6x6_game1.csv"
 
-# Card 16 zetten -> Breadth 16 zetten -> Depth ...
+# Card 16 zetten -> Breadth 16 zetten -> Depth 49
 # fp = "vehicles_6x6_game2.csv"
 
-# Card 16 zetten -> Breadth 16 -> Depth ...
-# fp = "vehicles_6x6_game3.csv"
+# Card 16 zetten -> Breadth 16 -> Depth 485
+fp = "vehicles_6x6_game3.csv"
 
-# Card 15 zetten -> Breadth 15 zetten -> Depth ...
+# Card 15 zetten -> Breadth 15 zetten -> Depth 160
 # fp = "vehicles_6x6_game4.csv"
 
-# Breadth ... -> Depth ...
+# Breadth 27 moves -> Depth ...
 # fp = "vehicles_9x9_1.csv"
 
 # Breadth ... -> Depth ...
@@ -40,7 +40,7 @@ fp = "vehicles_6x6_2.csv"
 # fp = "vehicles_9x9_3.csv"
 
 # Random's best: 175 zetten
-fp = "vehicles_12x12.csv"
+# fp = "vehicles_12x12.csv"
 
 def main():
     with open(fp, "rb") as fileRushhour:
@@ -48,8 +48,20 @@ def main():
         for vehicle in reader_vehicles:
             vehicles[vehicle[1]] = Vehicle(vehicle[0], vehicle[1], int(vehicle[2]), int(vehicle[3]), int(vehicle[4]))
 
-    Board1 = Board(12, 12, 5, vehicles)
-    DepthFirstSearch(Board1)
+    Board1 = Board(6, 6, 2, vehicles)
+    max_depth = 18
+    solution_list = []
+    while max_depth != -1:
+        print "Current max_depth: ", max_depth
+        max_depth, solution_list = DepthFirstSearch(Board1, max_depth, solution_list)
+        Board1 = Board(6, 6, 2, vehicles)
+
+    # iterate backwards over de parent boards in solution list
+    for i, boards in enumerate(solution_list[::-1]):
+        # print the number of moves
+        print "s: ", i
+        # print the board
+        boards.printBoard()
 
 if __name__ == "__main__":
     main()
